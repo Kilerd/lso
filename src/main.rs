62d8c2073e6e6026f998821c64d92f0cdf35c674
Kilerd Chan <blove694@gmail.com>
2024-01-16 16:58:48 +0800

    feat: execute explain sql for those non-select sql

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use anyhow::Result;
 use dotenv::dotenv;
 use futures_util::StreamExt;
+use log::info;
 use mysql_async::binlog::events::{QueryEvent, WriteRowsEvent};
 use mysql_async::binlog::EventType;
-use mysql_async::prelude::{Query, WithParams};
+use mysql_async::prelude::{Query, Queryable, WithParams};
 use mysql_async::{BinlogStreamRequest, Opts};
 
 #[derive(Debug)]
@@ -13,13 +14,31 @@ struct BinLogRow {
     encrypted: String,
 }
 
+#[derive(Debug)]
+struct ExplainResult {
+    id: u64,
+    select_type: String,
+    table: String,
+    partitions: Option<String>,
+    _type: String,
+    possible_keys: Option<String>,
+    key: Option<String>,
+    key_len: Option<u64>,
+    _ref: Option<String>,
+    rows: Option<u64>,
+    filtered: Option<f64>,
+    extra: Option<String>,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
+    pretty_env_logger::init();
     println!("Hello, world!");
     let opts = Opts::from_url(&std::env::var("SQL_DSN").expect("SQL_DSN must be set"))?;
     let pool = mysql_async::Pool::new(opts);
     let mut conn = pool.get_conn().await?;
+    let mut explain_conn = pool.get_conn().await?;
     let mut bin_logs = "SHOW BINARY LOGS"
         .with(())
         .map(&mut conn, |(log_name, file_size, encrypted)| BinLogRow {
@@ -28,7 +47,6 @@ async fn main() -> Result<()> {
             encrypted,
         })
         .await?;
-    dbg!(&bin_logs);
     let option = bin_logs.pop().expect("cannot get latest bin log file");
     let mut binlog = conn
         .get_binlog_stream(BinlogStreamRequest::new(1).with_filename(option.log_name.as_bytes()).with_pos(option.file_size))
@@ -40,7 +58,33 @@ async fn main() -> Result<()> {
             EventType::QUERY_EVENT => {
                 let query_event: QueryEvent = event.read_event()?;
 
-                println!("get event, {}", query_event.query());
+                let query = query_event.query().trim().to_string();
+                info!("[{}:{}] get event, `{}`", query_event.thread_id(), query_event.execution_time(), &query);
+                dbg!(&query);
+                if query.ne("BEGIN") && query.ne("COMMIT") {
+                    let explain_sql = format!("EXPLAIN {}", &query);
+                    info!("explaining sql: {}", &query);
+                    let explain_result = explain_sql
+                        .with(())
+                        .map(
+                            &mut explain_conn,
+                            |(id, select_type, table, partitions, _type, possible_keys, key, key_len, _ref, rows, filtered, extra)| ExplainResult {
+                                id,
+                                select_type,
+                                table,
+                                partitions,
+                                _type,
+                                possible_keys,
+                                key,
+                                key_len,
+                                _ref,
+                                rows,
+                                filtered,
+                                extra,
+                            },
+                        )
+                        .await?;
+                }
             }
             EventType::WRITE_ROWS_EVENT => {
                 let detail_event: WriteRowsEvent = event.read_event()?;
