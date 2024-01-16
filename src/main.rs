87bfcc3f4352e7f9cf2a66234e11360b14d09181
Kilerd Chan <blove694@gmail.com>
2024-01-16 14:32:14 +0800

    feat: handle new incoming events

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
-use mysql_async::{BinlogStreamRequest, Opts};
 use anyhow::Result;
 use dotenv::dotenv;
 use futures_util::StreamExt;
 use mysql_async::binlog::events::{QueryEvent, WriteRowsEvent};
 use mysql_async::binlog::EventType;
+use mysql_async::prelude::Query;
+use mysql_async::prelude::WithParams;
+use mysql_async::{BinlogStreamRequest, Opts};
+
+#[derive(Debug)]
+struct BinLogRow {
+    log_name: String,
+    file_size: u64,
+    encrypted: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -12,7 +21,23 @@ async fn main() -> Result<()> {
     let opts = Opts::from_url(&std::env::var("SQL_DSN").expect("SQL_DSN must be set"))?;
     let pool = mysql_async::Pool::new(opts);
     let mut conn = pool.get_conn().await?;
-    let mut binlog = conn.get_binlog_stream(BinlogStreamRequest::new(1)).await?;
+    let mut bin_logs = "SHOW BINARY LOGS"
+        .with(())
+        .map(&mut conn, |(log_name, file_size, encrypted)| BinLogRow {
+            log_name,
+            file_size,
+            encrypted,
+        })
+        .await?;
+    dbg!(&bin_logs);
+    let option = bin_logs.pop().expect("cannot get latest bin log file");
+    let mut binlog = conn
+        .get_binlog_stream(
+            BinlogStreamRequest::new(1)
+                .with_filename(option.log_name.as_bytes())
+                .with_pos(option.file_size),
+        )
+        .await?;
 
     while let Some(Ok(event)) = binlog.next().await {
         let eventtype = event.header().event_type()?;
@@ -26,6 +51,7 @@ async fn main() -> Result<()> {
                 let detail_event: WriteRowsEvent = event.read_event()?;
                 dbg!(detail_event);
             }
+            // todo: handle bin log file switch event
             _ => {}
         }
     }
